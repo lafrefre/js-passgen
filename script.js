e32af43c5f525346b4830b1d991bc20e0b23f28f
lafrefre <134004859+lafrefre@users.noreply.github.com>
2023-07-04 15:19:17 -0700

    "final commits"

--- script.js
+++ script.js
@@ -13,7 +13,7 @@ function writePassword() {
 
 // im going to be using function and see if it works or not//
 function generatePassword() {
-  var makePassWordlength = makePassWordlength(14);
+  var makePassWordlength = makePassWordlength(0);
   if (!makePassWordlength) return " ";
 
   var includSpecials = confirm("Include special characters.");
@@ -33,13 +33,13 @@ function generatePassword() {
   var numericChars = "0123456789";
   // adding specifications for what can be used for the random password generatir to give us a large area way to make a unique and good password. 
 
-  var avaliableChara = " ";
-  if (includeSpecial) availableChars += specialChars;
-  if (includeLowercase) availableChars += lowercaseChars;
+  var availableChars = " ";
+  if (includSpecials) availableChars += specialChars;
+  if (includeLowcase) availableChars += lowercaseChars;
   if (includeUppercase) availableChars += uppercaseChars;
   if (includeSpecial) availableChars += specialChars;
 
-  var generratedPassword = " ";
+  var generatedPassword = " ";
   for (var i = 0; i < makePassWordlength; i++) {
     var randomIndex = math.floor(Math.random() * avaliableChara.length);
     generatePassword += avaliableChara[randomIndex];
@@ -49,7 +49,17 @@ function generatePassword() {
 
 }
 
-// Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
+function promtPasswordLength() {
+  var passWordlengthStar = promt("Please enter a password length between 10 and 100 characters long.");
+  var promptPasswordLength = parseInt(passWordlengthStar);
+
+  if (isNaN(passWordlength) || passWordlength < 10 || passWordlength > 100) {
+    alert("Please choose a valid password length between 10 and 100 characters long.");
+    return null;
+  }
+  
+  return passWordlength;
+}
+
 
 
